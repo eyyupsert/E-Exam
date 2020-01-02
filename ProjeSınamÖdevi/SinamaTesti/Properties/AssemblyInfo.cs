f07d462fff04751a894e701d72944e29b16a160f
eyyupsert <eyyupsert@outlook.com>
2020-01-02 13:26:09 +0300

    Güncellendi

---
+++ ProjeSınamÖdevi/SinamaTesti/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SinamaTesti")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SinamaTesti")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e4d9643e-87e3-404e-9dcc-2100eb6bc878")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ProjeSınamÖdevi/SinamaTesti/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusinessLibrary;
+using CommonLibrary;
+using System.Collections.Generic;
+
+namespace SinamaTesti
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void SoruGoruntulemeTest()
+        {
+            SorularBusiness soruBll = new SorularBusiness();
+            List<Sorular> sorular = soruBll.SoruGoruntule();
+            Assert.IsNotNull(sorular);
+        }
+
+        [TestMethod]
+        public void SinavSonuclariTest()
+        {
+            SinavBilgileriBusiness sinavBll = new SinavBilgileriBusiness();
+            List<SinavBilgileri> sinavlar = sinavBll.SinavIcerigi(0);
+            Assert.IsNotNull(sinavlar);
+        }
+
+        [TestMethod]
+        public void OgretmenSonucGorunmtulumeTesti()
+        {
+            SinavBilgileriBusiness sonuclarBLL = new SinavBilgileriBusiness();
+            List<SinavBilgileri> sonuclar = sonuclarBLL.OgretmenSonuclariGoruntule();
+            Assert.IsNotNull(sonuclar);
+        }
+
+    }
+}
